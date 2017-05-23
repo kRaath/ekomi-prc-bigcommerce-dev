b58b865f0b4ff0b77b350de72eca7af92375718f
kRaath <khadim.raath@gmail.com>
2017-05-23 17:18:03 +0500

    widgets.js added

---
+++ api/resources/scripts/widgets.js
@@ -0,0 +1,40 @@
+
+function miniStarsWidget(){
+
+$(".ekomi-miniStars").each(function() { 
+
+        var productId = $(this).data('product-id');
+        var storehash = $(this).data('storehash');
+         
+        var item;
+        item =this;
+
+        $.ajax({
+            type: "GET",
+            url: 'http://localhost/ekomi-prc-bigcommerce/api/v1/miniStarsWidget?storeHash='+storehash+'&productId='+productId,
+            data: null,
+            cache: false,
+            success: function (data) {
+              $(item).html(data);
+            }
+        });
+});
+}
+
+function reviewsContainerWidget(){
+        var storehash = $('.ekomi-prc').data('storehash');
+        var productId = $('.ekomi-prc').data('product-id');
+
+        $.ajax({
+            type: "GET",
+             url: 'http://localhost/ekomi-prc-bigcommerce/api/v1/reviewsContainerWidget?storeHash='+storehash+'&productId='+productId,
+            data: null,
+            cache: false,
+            success: function (data) {
+              $('.ekomi-prc').html(data);
+            }
+        });
+}
+
+miniStarsWidget();
+reviewsContainerWidget();
