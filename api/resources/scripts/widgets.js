868f2c86dcccdf1fe00a746ac179fa853893dfd4
kRaath <khadim.raath@gmail.com>
2017-05-25 18:12:34 +0500

    google structured data fix

--- api/resources/scripts/widgets.js
+++ api/resources/scripts/widgets.js
@@ -5,22 +5,23 @@ function miniStarsWidget() {
 
         var productId = $(this).data('product-id');
         var storehash = $(this).data('storehash');
+        if (storehash != '' && productId != '') {
+            var item;
+            item = this;
 
-        var item;
-        item = this;
-
-        $.ajax({
-            type: "GET",
-            url: 'https://plugindev.coeus-solutions.de/ekomi-prc-bigcommerce-dev/api/v1/miniStarsWidget?storeHash=' + storehash + '&productId=' + productId,
-            data: null,
-            cache: false,
-            success: function (data) {
-                if (data) {
-                    var response = $.parseJSON(data);
-                    $(item).html(response.widgetHtml);
+            $.ajax({
+                type: "GET",
+                url: 'https://plugindev.coeus-solutions.de/ekomi-prc-bigcommerce-dev/api/v1/miniStarsWidget?storeHash=' + storehash + '&productId=' + productId,
+                data: null,
+                cache: false,
+                success: function (data) {
+                    if (data) {
+                        var response = $.parseJSON(data);
+                        $(item).html(response.widgetHtml);
+                    }
                 }
-            }
-        });
+            });
+        }
     });
 }
 
@@ -28,20 +29,22 @@ function reviewsContainerWidget() {
     var storehash = $('.ekomi-prc').data('storehash');
     var productId = $('.ekomi-prc').data('product-id');
 
-    $.ajax({
-        type: "GET",
-        url: 'https://plugindev.coeus-solutions.de/ekomi-prc-bigcommerce-dev/api/v1/reviewsContainerWidget?storeHash=' + storehash + '&productId=' + productId,
-        data: null,
-        cache: false,
-        success: function (data) {
-            if (data) {
-                var response = $.parseJSON(data);
-                $('.ekomi-prc').html(response.widgetHtml);
-                var script = response.jsonld;
-                $(".ekomi-prc").append('<script type="application/ld+json">' + script + "</script>");
+    if (storehash != '' && productId != '') {
+        $.ajax({
+            type: "GET",
+            url: 'https://plugindev.coeus-solutions.de/ekomi-prc-bigcommerce-dev/api/v1/reviewsContainerWidget?storeHash=' + storehash + '&productId=' + productId,
+            data: null,
+            cache: false,
+            success: function (data) {
+                if (data) {
+                    var response = $.parseJSON(data);
+                    $('.ekomi-prc').html(response.widgetHtml);
+                    var script = response.jsonld;
+                    $(".ekomi-prc").append('<script type="application/ld+json">' + script + "</script>");
+                }
             }
-        }
-    });
+        });
+    }
 }
 
 miniStarsWidget();
