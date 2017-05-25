ac0af67643f8758dbb529740b4a6aa0ca489356f
kRaath <khadim.raath@gmail.com>
2017-05-25 18:16:29 +0500

    widgets.js fix

--- api/resources/scripts/widgets.js
+++ api/resources/scripts/widgets.js
@@ -5,7 +5,7 @@ function miniStarsWidget() {
 
         var productId = $(this).data('product-id');
         var storehash = $(this).data('storehash');
-        if (storehash != 'undefined' && productId != 'undefined') {
+        if (storehash !== 'undefined' && productId !== 'undefined') {
             var item;
             item = this;
 
@@ -29,7 +29,7 @@ function reviewsContainerWidget() {
     var storehash = $('.ekomi-prc').data('storehash');
     var productId = $('.ekomi-prc').data('product-id');
 
-    if (storehash != 'undefined' && productId != 'undefined') {
+    if (storehash !== 'undefined' && productId !== 'undefined') {
         $.ajax({
             type: "GET",
             url: 'https://plugindev.coeus-solutions.de/ekomi-prc-bigcommerce-dev/api/v1/reviewsContainerWidget?storeHash=' + storehash + '&productId=' + productId,
