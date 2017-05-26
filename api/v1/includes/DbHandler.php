549abed916e31af1e1bc4e9ff349434c179860cd
kRaath <khadim.raath@gmail.com>
2017-05-26 16:00:31 +0500

    fix avg stars

--- api/v1/includes/DbHandler.php
+++ api/v1/includes/DbHandler.php
@@ -219,7 +219,7 @@ class DbHandler {
   "@type": "Product",
   "aggregateRating": {
     "@type": "AggregateRating",
-    "ratingValue": "' . $data['avgStars'] . '",
+    "ratingValue": "' . number_format($data['avgStars'], 2) . '",
     "reviewCount": "' . $data['reviewsCountTotal'] . '"
   },
   "productID": "' . $data['productId'] . '",
